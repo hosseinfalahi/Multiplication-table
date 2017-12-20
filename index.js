0ae4d4055d1c0eb09257254122b15e27fe03b62c
AmirHossein Falahi <hossein.falahi@gmail.com>
2017-12-21 02:17:37 +0330

    converting code to function

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-/*console.log("hello world");
-var x = 14;
-if (x % 2 == 0)
-    console.log("even");
-else 
-    console.log('odd');*/
-var x = 10;
-var y = 10;
-var line = '';
-for (var i = 1;i <= x;i++){
-    for (var j = 1;j <= y;j++){
-        line = line + (i * j);
-        if (i * j < 10)
-            line = line + '  '
-        else 
-            line = line + ' '
+function getMultiplicationTable (num){
+    var x = num;
+    var y = num;
+    var line = '';
+    for (var i = 1;i <= x;i++){
+        for (var j = 1;j <= y;j++){
+            line = line + (i * j);
+            if (i * j < 10)
+                line = line + '  '
+            else 
+                line = line + ' '
+        }
+        line = line + '\n';
     }
-    console.log(line);
-    line = '';
+    return line;
 }
-    
+/*
+sample usage:
+console.log(getMultiplicationTable(10));
+*/
