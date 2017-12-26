6edcff2d9c382c8966c80db7c11302bb8e225040
AmirHossein Falahi <hossein.falahi@gmail.com>
2017-12-27 02:42:05 +0330

    solving issue #2 (fixing separator bug)

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ function getMultiplicationTable (num){
     var x = num;
     var y = num;
     var line = '';
+    var sq = x * y;
+    var separator = sq.toString().length;
     for (var i = 1;i <= x;i++){
         for (var j = 1;j <= y;j++){
             line = line + (i * j);
-            if (i * j < 10)
-                line = line + '  '
-            else 
-                line = line + ' '
+            sq2 = i * j;
+            var adad = sq2.toString().length;
+            for (var k = adad;k <= separator;k++)
+                line = line + ' ';
         }
         line = line + '\n';
     }
